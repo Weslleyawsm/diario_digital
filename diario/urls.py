5833f44cd775ea12c215ef6e207e768499146123
Weslleyawsm <weslleysheshe@gmail.com>
2025-10-07 09:29:11 -0300

    Update urls.py

--- diario/urls.py
+++ diario/urls.py
@@ -3,10 +3,11 @@ from . import views, services
 
 urlpatterns = [
     path('', views.pagina_diario, name='pagina_diario'),
-    path('api/tarefa/adicionar/', views.adicionar_tarefas),
-    path('api/tarefa/toggle/', views.toggle_tarefa),
-    path('api/tarefa/atualizar-descricao/', views.atualizar_descricao),
-    path('api/tarefa/remover/', views.remover_tarefa),
-    path('api/diario/campos-reflexao/', views.campos_gratidao),
-    path('api/diario/tarefas-futuras/', views.definir_tarefas_futuras),
+    path('api/tarefa/adicionar/', views.adicionar_tarefas, name='adicionar_tarefas'),
+    path('api/tarefa/toggle/', views.toggle_tarefa, name='toggle_tarefa'),
+    path('api/tarefa/atualizar-descricao/', views.atualizar_descricao, name='atualizar_descricao'),
+    path('api/tarefa/remover/', views.remover_tarefa, name='remover_tarefa'),
+    path('api/diario/campos-reflexao/', views.campos_gratidao, name='campos_gratidao'),
+    path('api/diario/tarefas-futuras/', views.definir_tarefas_futuras, name='definir_tarefas_futuras'),
+    path('api/diario/consultar-tarefas/', views.consultar_tarefas, name='consultar_tarefas'),
 ]
